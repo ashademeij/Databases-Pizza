4088b1d9ccfaf695a203a8cf53e51de9ec817c8b
michellefilipaa <72268318+michellefilipaa@users.noreply.github.com>
2021-10-06 11:15:47 +0200

    Update CreateAllTables.java

--- Tables/CreateAllTables.java
+++ Tables/CreateAllTables.java
@@ -1,19 +1,15 @@
 package Tables;
 
-import java.sql.Connection;  
-import java.sql.DriverManager;  
-import java.sql.SQLException;  
-import java.sql.Statement;  
-
 public class CreateAllTables{
     public static void main(String[] args){
-        CreateCustomerTable.createNewTable(); //THIS WORKS YAYYY
-        CreateDeliveryTable.createNewTable();
+        CreateCustomerTable.createNewTable(); 
+        CreateDeliveryTable.createNewTable(); //FIX TIME
         CreateEmployeeTable.createNewTable();
         CreateNewTableDesserts.createNewTable();
-        CreateNewTableDrinks.createNewTable();  //THIS WORKS YAYYY
-        CreateNewTablePizza.createNewTable();
-        CreateNewTableTopping.createNewTable();
+        CreateNewTableDrinks.createNewTable(); 
+        CreateNewTablePizza.createNewTable(); //FIX THIS
+        CreateNewTableTopping.createNewTable(); 
+        CreateOrderTable.createNewTable();
 
     }
 }
