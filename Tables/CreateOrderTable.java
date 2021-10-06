cd41ad96ca806ef7fdf14bd4dda0a71eeb52ccd2
michellefilipaa <72268318+michellefilipaa@users.noreply.github.com>
2021-10-06 11:18:28 +0200

    Update CreateOrderTable.java

--- Tables/CreateOrderTable.java
+++ Tables/CreateOrderTable.java
@@ -9,22 +9,23 @@ public class CreateOrderTable {
    
     public static void createNewTable() {  
         // SQLite connection string  
-        String url = "jdbc:sqlite:/Users/helendemeij/Downloads/Databases-Pizza/Pizza_Shop.db";  
+        String url = "jdbc:mysql://localhost/Pizza_Shop?user=root&password=asha1234";  
           
         // SQL statement for creating a new table  
         String sql = "CREATE TABLE IF NOT EXISTS orders ("  
-                + " order_id integer PRIMARY KEY NOT NULL"
-                + " pizza text NOT NULL"
-                + " drinks text NOT NULL"  
-                + " dessert text NOT NULL"
-                + " discount integer NOT NULL"
-                + " total integer NOT NULL"
+                + " order_id INTEGER PRIMARY KEY NOT NULL,"
+                + " pizza TEXT,"
+                + " drinks TEXT,"  
+                + " dessert TEXT,"
+                + " discount INTEGER NOT NULL DEFAULT 0,"
+                + " total INTEGER NOT NULL"
                 + ");";    
           
         try{  
             Connection conn = DriverManager.getConnection(url);  
             Statement stmt = conn.createStatement();  
             stmt.execute(sql);  
+            System.out.println("ORDER table created");
         } catch (SQLException e) {  
             System.out.println(e.getMessage() +"ORDERS");  
         }  
