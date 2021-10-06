33bab2e24e39f285202683ae02d86530f0b65d75
michellefilipaa <72268318+michellefilipaa@users.noreply.github.com>
2021-10-06 11:17:36 +0200

    Update CreateNewTableDesserts.java

--- Tables/CreateNewTableDesserts.java
+++ Tables/CreateNewTableDesserts.java
@@ -5,15 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class CreateNewTableDrinks {
+public class CreateNewTableDesserts {
     public static void createNewTable() {
         // SQLite connection string
         String url = "jdbc:mysql://localhost/Pizza_Shop?user=root&password=asha1234";
 
         // SQL statement for creating a new table
-        String sql = "CREATE TABLE IF NOT EXISTS drink ("
-                + " drink_id INTEGER PRIMARY KEY NOT NULL,"
-                + " name TEXT NOT NULL, "
+        String sql = "CREATE TABLE IF NOT EXISTS dessert ("
+                + " desserts_id INTEGER PRIMARY KEY AUTO_INCREMENT,"
+                + " name TEXT NOT NULL,"
                 + " cost_price INTEGER NOT NULL"
                 + ");";
 
@@ -21,9 +21,9 @@ public class CreateNewTableDrinks {
             Connection conn = DriverManager.getConnection(url);
             Statement stmt = conn.createStatement();
             stmt.execute(sql);
-            System.out.println("DRINKS TABLE IS CREATED");
+            System.out.println("DESSERTS TABLE IS CREATED");
         } catch (SQLException e) {
-            System.out.println(e.getMessage() + " Drinks");
+            System.out.println(e.getMessage() + "Desserts");
         }
     }
 
