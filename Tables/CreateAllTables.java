7cca6d89e2575f57a7cf24c91d2aef4c65a4339c
michellefilipaa <m.lear@student.maastrichtuniversity.nl>
2021-10-11 22:22:41 +0200

    finito

--- Tables/CreateAllTables.java
+++ Tables/CreateAllTables.java
@@ -2,14 +2,14 @@ package Tables;
 
 public class CreateAllTables{
     public static void main(String[] args){
-        //CreateCustomerTable.createNewTable(); 
-        //CreateDeliveryTable.createNewTable(); //FIX TIME
-        //CreateEmployeeTable.createNewTable();
-        //CreateNewTableDesserts.createNewTable();
-        //CreateNewTableDrinks.createNewTable(); 
-        CreateNewTablePizza.createNewTable(); //FIX THIS
-        //CreateNewTableTopping.createNewTable(); 
-        //CreateOrderTable.createNewTable();
+        CreateCustomerTable.createNewTable(); 
+        CreateDeliveryTable.createNewTable(); 
+        CreateEmployeeTable.createNewTable();
+        CreateNewTableDesserts.createNewTable();
+        CreateNewTableDrinks.createNewTable(); 
+        CreateNewTablePizza.createNewTable(); 
+        CreateNewTableTopping.createNewTable(); 
+        CreateOrderTable.createNewTable();
 
     }
 }
